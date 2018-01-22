7300fdc86308f331e5c3bd400a9ea6e34472883d
nanomanon56 <manon.l.jean@gmail.com>
2018-01-22 09:31:01 -0500

    fixed add plays and added categories

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,51 +1,63 @@
 class PlaysController < ApplicationController
-  before_action :find_play, only: [:show, :edit, :update, :destroy]
+	before_action :find_play, only: [:show, :edit, :update, :destroy]
+	#before_action :authenticate_user!, only: [:new, :edit]
 
-  def index
-    @plays = Play.all.order("created_at DESC")
-  end
-
-  def show
-  end
-
-  def new
-    @play = current_user.play.build
-  end
-
-  def create
-    @play = current_user.play.build(play_params)
-    if @play.save
-      redirect_to root_path
-      #refresh the page and the data is lost if not saved
-    else
-      render 'new'
-      #render does not refresh the page, left to save if fails
-    end
-
-  def edit
-  end
-
-  def update
-     if @play.update(play_params)
-       redirect_to play_path(@play)
-    else
-      render 'edit'
-    end
- end
-
-  def destroy
-    @play.destroy
-    redirect_to root_path
-  end
+	def index
+		if params[:category].blank?
+			@plays = Play.all.order("created_at DESC")
+		#else
+			#@category_id = Category.find_by(name: params[:category]).id
+			#@plays = Play.where(:category_id => @category_id).order("created_at DESC")
+		#end
+	end
 
 end
+	def show
+	end
 
-private
+	def new
+		@play = current_user.plays.build
+		@categories = Category.all.map{ |c| [c.name, c.id] }
+	end
 
-def play_params
-  params.require(:play).permit(:title, :decription, :director)
-end
-  def find_play
-    @play = Play.find(params[:id])
-  end
+	def create
+		@play = current_user.plays.build(play_params)
+		@play.category_id = params[:category_id]
+
+		if @play.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@categories = Category.all.map{ |c| [c.name, c.id] }
+	end
+
+	def update
+		@play.category_id = params[:category_id]
+
+		if @play.update(play_params)
+			redirect_to play_path(@play)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@play.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def play_params
+		params.require(:play).permit(:title, :description, :director, :category_id, :play_img)
+	end
+
+	def find_play
+		@play = Play.find(params[:id])
+	end
+
 end
